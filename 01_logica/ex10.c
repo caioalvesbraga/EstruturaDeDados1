de53ad3ae9f5f2b67ae586420b9e666f383c3786
meanmustard <caio.braga.felipe@gmail.com>
2022-11-22 19:06:37 -0300

    Nova correção nos nomes dos arquivos

--- 01_logica/ex10.c
+++ 01_logica/ex10.c
@@ -1,219 +1,107 @@
 #include <stdio.h>
 
-#define MAXL 1000
-#define MAXC 11
-
-char matriz[MAXL][MAXC];
-void initiateMatriz(int L);
-void printmatriz(int L);
-void setTree(char G_lado, int G_linha);
-char changeLumberjack(char Lado_lenhador, int L);
-int breakTree(char Lado_lenhador, int L);
-void attmatriz(char Lado_lenhador, int L);
-void setLumberjack(char Lado_lenhador);
-
-int main(int argc, char const *argv[]){
-
-    int L, G, G_linha, i=0, quebrado=0 ;
-    char comands[2000], G_lado, Lado_lenhador;
-
-    scanf("%d", &L);
-    getchar();
-
-    initiateMatriz(L);
-
-    scanf("%d", &G);
-    getchar();
-
-    for(int i = 0 ; i < G ; i++){
-
-        scanf("%c %d", &G_lado, &G_linha);
-        getchar();
-
-        setTree(G_lado,G_linha);
-
+void desenhaArvore(char matriz[][11], int altura)
+{
+  printf("~~~~~~~~~~~\n");
+  for (int i = altura + 4; i >= altura; i--){
+    for (int j = 0; j < 11; j++){
+      if (matriz[i][j] == 0){
+        printf(" ");
+      }
+      else{
+        printf("%c", matriz[i][j]);
+      }
     }
+    printf("\n");
+  }
+  printf("~~~~~~~~~~~\n");
+}
 
-    scanf("%c", &Lado_lenhador);
-    getchar();
+int main()
+{
+  int totalInputs, totalGalhos, sentido = 0, altura = 0;
+  char matriz[3000][11], sentidoInicial, movimentos;
 
-    setLumberjack(Lado_lenhador);
+  scanf("%i", &totalInputs);
+  for (int i = 0; i < totalInputs; i++){
+    for (int j = 4; j < 7; j++){
+      matriz[i][j] = '|';
+    }
+  }
 
-    printmatriz(L);
+  scanf("%i", &totalGalhos);
+  for (int i = 0; i < totalGalhos; i++){
+    char lado;
+    int linha;
+    scanf(" %c %i", &lado, &linha);
 
-    scanf("%s", comands);
+    int positionGalho = linha - 1;
+    if (lado == 'D'){
+      matriz[positionGalho][7] = '-';
+      matriz[positionGalho][8] = '-';
+      matriz[positionGalho][9] = '-';
+    }
+    else{
+      matriz[positionGalho][1] = '-';
+      matriz[positionGalho][2] = '-';
+      matriz[positionGalho][3] = '-';
+    }
+  }
 
-    while(comands[i]!='\0'){
+  scanf(" %c", &sentidoInicial);
+  if (sentidoInicial == 'D'){
+    sentido = 1;
+    matriz[0][8] = 'L';
+    matriz[1][8] = 'L';
+  }
+  else{
+    sentido = -1;
+    matriz[0][2] = 'L';
+    matriz[1][2] = 'L';
+  }
 
-        if(comands[i] == 'T') Lado_lenhador = changeLumberjack(Lado_lenhador, L);
 
-        if( comands[i] == 'B' ) quebrado = breakTree(Lado_lenhador, L);
+  desenhaArvore(matriz, altura);
+  scanf("%c", &movimentos);
+  while (scanf("%c", &movimentos) > 0){
+    int zonaPerigo = 5 - sentido * 3;
+    int zonaAtual = 5 + sentido * 3;
 
-        if(quebrado == 1 ){
+    if (movimentos == 'T'){
+      if(matriz[altura+1][zonaPerigo] == '-' || matriz[altura][zonaPerigo]) {
+        printf("**beep**\n");
+        continue;
+      }
 
-            printf("*morreu*\n");
-            return 0;
+      matriz[altura][zonaPerigo] = 'L';
+      matriz[altura + 1][zonaPerigo] = 'L';
+      matriz[altura][zonaAtual] = '\0';
+      matriz[altura + 1][zonaAtual] = '\0';
 
+      sentido *= -1;
+    }
+    else if(movimentos == 'B'){
+      int sinal = 5 + sentido;
+      char sinalQuebra = matriz[altura][sinal];
+      if (sinalQuebra == 61 - sentido)
+      {
+        altura++;
+        if(matriz[altura+1][zonaAtual] == '-') {
+          printf("**morreu**\n");
+          return 0;
         }
 
-        i++;
-
+        matriz[altura+1][zonaAtual] = 'L';
+      }
+      else{
+        matriz[altura][sinal] = 61 - sentido;
+      }
+    } else {
+      continue;
     }
 
-
-    return 0;
-}
-
-//---------------------------------------
-
-void initiateMatriz(int L){
-
-    for(int i = 0 ; i < MAXL ; i++){
-
-        for(int j = 0 ; j < MAXC ; j++){
-
-            if( j >= 4 && j <=6 && i >= MAXL - L) matriz[i][j] = '|';
-
-            else matriz[i][j] = ' ';
-
-        }
-
-    }
-
-}
-
-void printmatriz(int L){
-
-    printf("~~~~~\n");
-
-    for(int i = MAXL - 5  ; i < MAXL ; i++){
-
-        for(int j = 0 ; j < MAXC ; j++){
-
-            printf("%c", matriz[i][j]);
-
-        }
-
-        printf("\n");
-
-    }
-
-    printf("~~~~~\n");
-
-}
-
-void setTree (char G_lado, int G_linha) {
-
-    if ( G_lado == 'D'){
-
-        matriz[MAXL-G_linha][8] = '-';
-        matriz[MAXL-G_linha][9] = '-';
-        matriz[MAXL-G_linha][10] = '-';
-
-    } else if ( G_lado == 'E' ){
-
-        matriz[MAXL-G_linha][0] = '-';
-        matriz[MAXL-G_linha][1] = '-';
-        matriz[MAXL-G_linha][2] = '-';
-
-    }
-
-}
-
-void setLumberjack(char Lado_lenhador){
-
-    if ( Lado_lenhador == 'D' ) {
-
-        matriz[MAXL-2][8] = 'L';
-        matriz[MAXL-1][8] = 'L';
-
-    } else if ( Lado_lenhador == 'E' ) {
-
-        matriz[MAXL-2][2] = 'L';
-        matriz[MAXL-1][2] = 'L';
-
-    }
-
-}
-
-char changeLumberjack(char Lado_lenhador, int L){
-
-        if( Lado_lenhador == 'D'){
-
-            if( matriz[MAXL-2][2] == '-' || matriz[MAXL-1][2] == '-' ){
-
-                printf("*beep*\n");
-                return Lado_lenhador;
-
-            } else {
-
-                matriz[MAXL-2][2] = 'L';
-                matriz[MAXL-1][2] = 'L';
-                matriz[MAXL-1][8] = ' ';
-                matriz[MAXL-2][8] = ' ';
-                Lado_lenhador = 'E';
-                printmatriz(L);
-                return Lado_lenhador;
-
-            }
-
-        } else if( Lado_lenhador == 'E'){
-
-            if( matriz[MAXL-2][8] == '-' || matriz[MAXL-1][8] == '-' ){
-
-                printf("*beep*\n");
-                return Lado_lenhador;
-
-            } else {
-
-                matriz[MAXL-2][8] = 'L';
-                matriz[MAXL-1][8] = 'L';
-                matriz[MAXL-1][2] = ' ';
-                matriz[MAXL-2][2] = ' ';
-                Lado_lenhador = 'D';
-                printmatriz(L);
-                return Lado_lenhador;
-
-            }
-        }
-
-}
-
-int breakTree(char Lado_lenhador, int L){
-
-        if(Lado_lenhador == 'D' && matriz[MAXL-1][6] == '|') matriz[MAXL-1][6] = '<';
-
-        else if(Lado_lenhador == 'E' && matriz[MAXL-1][4] == '|') matriz[MAXL-1][4] = '>';
-
-        else if(Lado_lenhador == 'D' && matriz[MAXL-1][6] == ' ') return 0;
-
-        else if(Lado_lenhador == 'E' && matriz[MAXL-1][4] == ' ') return 0;
-
-        else if(Lado_lenhador == 'D' && matriz[MAXL-1][6] == '<' && matriz[MAXL-3][10]!='-') attmatriz(Lado_lenhador, L);
-
-        else if(Lado_lenhador == 'E' && matriz[MAXL-1][4] == '>' && matriz[MAXL-3][1]!='-') attmatriz(Lado_lenhador, L);
-
-        else return 1;
-
-        printmatriz(L);
-
-    return 0;
-
-}
-
-void attmatriz(char Lado_lenhador, int L){
-
-    for(int i = MAXL ; i > MAXL - L -1; i--) {
-
-        for(int j = 0 ; j < MAXC ; j++){
-
-            matriz[i][j] = matriz[i-1][j];
-
-
-        }
-
-    }
-
-    setLumberjack(Lado_lenhador);
-
+    desenhaArvore(matriz, altura);
+  }
+  printf("\n");
+  return 0;
 }
--- 01_logica/ex11.c
+++ 01_logica/ex11.c
@@ -1,107 +1,46 @@
 #include <stdio.h>
+#include <string.h>
 
-void desenhaArvore(char matriz[][11], int altura)
+int main(void)
 {
-  printf("~~~~~~~~~~~\n");
-  for (int i = altura + 4; i >= altura; i--){
-    for (int j = 0; j < 11; j++){
-      if (matriz[i][j] == 0){
-        printf(" ");
-      }
-      else{
-        printf("%c", matriz[i][j]);
-      }
-    }
-    printf("\n");
-  }
-  printf("~~~~~~~~~~~\n");
-}
+  int i = 0;
+  int lines;
+  scanf("%i", &lines);
+  char text[lines][1001];
+  int A, B;
 
-int main()
-{
-  int totalInputs, totalGalhos, sentido = 0, altura = 0;
-  char matriz[3000][11], sentidoInicial, movimentos;
-
-  scanf("%i", &totalInputs);
-  for (int i = 0; i < totalInputs; i++){
-    for (int j = 4; j < 7; j++){
-      matriz[i][j] = '|';
-    }
+  for (int i = 0; i < lines; i++)
+  {
+    scanf(" %10000[^\n]", text[i]);
   }
 
-  scanf("%i", &totalGalhos);
-  for (int i = 0; i < totalGalhos; i++){
-    char lado;
-    int linha;
-    scanf(" %c %i", &lado, &linha);
+  scanf("%i %i", &A, &B);
 
-    int positionGalho = linha - 1;
-    if (lado == 'D'){
-      matriz[positionGalho][7] = '-';
-      matriz[positionGalho][8] = '-';
-      matriz[positionGalho][9] = '-';
-    }
-    else{
-      matriz[positionGalho][1] = '-';
-      matriz[positionGalho][2] = '-';
-      matriz[positionGalho][3] = '-';
-    }
-  }
-
-  scanf(" %c", &sentidoInicial);
-  if (sentidoInicial == 'D'){
-    sentido = 1;
-    matriz[0][8] = 'L';
-    matriz[1][8] = 'L';
-  }
-  else{
-    sentido = -1;
-    matriz[0][2] = 'L';
-    matriz[1][2] = 'L';
-  }
-
-
-  desenhaArvore(matriz, altura);
-  scanf("%c", &movimentos);
-  while (scanf("%c", &movimentos) > 0){
-    int zonaPerigo = 5 - sentido * 3;
-    int zonaAtual = 5 + sentido * 3;
-
-    if (movimentos == 'T'){
-      if(matriz[altura+1][zonaPerigo] == '-' || matriz[altura][zonaPerigo]) {
-        printf("**beep**\n");
-        continue;
-      }
-
-      matriz[altura][zonaPerigo] = 'L';
-      matriz[altura + 1][zonaPerigo] = 'L';
-      matriz[altura][zonaAtual] = '\0';
-      matriz[altura + 1][zonaAtual] = '\0';
-
-      sentido *= -1;
-    }
-    else if(movimentos == 'B'){
-      int sinal = 5 + sentido;
-      char sinalQuebra = matriz[altura][sinal];
-      if (sinalQuebra == 61 - sentido)
+  char action;
+  while (scanf(" %c", &action) > 0)
+  {
+    if (action == 'j')
+    {
+      if (A < lines)
       {
-        altura++;
-        if(matriz[altura+1][zonaAtual] == '-') {
-          printf("**morreu**\n");
-          return 0;
-        }
-
-        matriz[altura+1][zonaAtual] = 'L';
+        A++;
       }
-      else{
-        matriz[altura][sinal] = 61 - sentido;
-      }
-    } else {
-      continue;
     }
+    else if (action == 'k')
+    {
+      if (A > 1)
+      {
+        A--;
+      }
+    }
+    int sizeL = strlen(text[A - 1]);
 
-    desenhaArvore(matriz, altura);
+    if (sizeL < B)
+    {
+      printf("%i %i %c\n", A, sizeL, text[A - 1][sizeL - 1]);
+    }
+    else
+      printf("%i %i %c\n", A, B, text[A - 1][B - 1]);
   }
-  printf("\n");
   return 0;
 }
